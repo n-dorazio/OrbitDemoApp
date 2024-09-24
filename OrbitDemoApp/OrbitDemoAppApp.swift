801bba1639b17be801e0644075190eab6922a96a
Zionate7 <natedorazio@gmail.com>
2024-09-24 11:37:59 -0600

    Initial Commit

---
+++ OrbitDemoApp/OrbitDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitDemoAppApp.swift
+//  OrbitDemoApp
+//
+//  Created by Nathaniel D'Orazio on 2024-09-24.
+//
+
+import SwiftUI
+
+@main
+struct OrbitDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrbitDemoAppTests/OrbitDemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitDemoAppTests.swift
+//  OrbitDemoAppTests
+//
+//  Created by Nathaniel D'Orazio on 2024-09-24.
+//
+
+import Testing
+@testable import OrbitDemoApp
+
+struct OrbitDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
